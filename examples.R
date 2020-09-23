0546dae0ebdca1c0cfea087975d94110e6fcd078
lucas jeay <jeaybizot@chapman.edu>
2020-09-23 11:35:31 +0200

    examples of uses in R

---
+++ examples.R
@@ -0,0 +1,11 @@
+###########################################################
+# THIS FILE INSTANTIATES USES OF THE MATLAB PACKAGE FOR R #
+###########################################################
+
+# ceil() function
+# FUNCTION: this function will round up to the nearest integer
+ceil(3.14159)
+
+# floor() function
+# FUNCTION: this function will round down to the nearest integer
+floor(3.14159)
