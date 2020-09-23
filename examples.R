6fc2263f9a5710189634ff49fbeb8e3771d066a2
lucas jeay <jeaybizot@chapman.edu>
2020-09-23 11:51:01 +0200

    examples of uses in R

--- examples.R
+++ examples.R
@@ -2,10 +2,23 @@
 # THIS FILE INSTANTIATES USES OF THE MATLAB PACKAGE FOR R #
 ###########################################################
 
-# ceil() function
+# Call the library to use the package
+library("matlab")
+
+# ceil()
 # FUNCTION: this function will round up to the nearest integer
 ceil(3.14159)
 
-# floor() function
+# floor()
 # FUNCTION: this function will round down to the nearest integer
 floor(3.14159)
+
+# zeros(), ones() and eye()
+# FUNCTION: this function will generate 0,1 and identity matrices
+
+  # squared 3x3 zero matrix
+  zero(3)
+  # 4x4 identity matrix
+  eye(4)
+  # 2x3 one matrix
+  ones(2,3)
